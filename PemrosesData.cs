995e1fc70db3b25b74a98677922e72d002e10988
unknown <fathanPrayoga>
2025-03-18 07:30:28 +0700

    menambahkan class pemrosesan data dengan method DapatkanNilaiTerbesar

---
+++ PemrosesData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace jurnalModul5_103022330070
+{
+    internal class PemrosesData
+    {
+        public void DapatkanNilaiTerbesar<T>(T nilai1, T nilai2, T nilai3 ) where T: IComparable
+        {
+            dynamic max = nilai1;
+            if (nilai2.CompareTo(max) > 0) max = nilai2;
+            if (nilai3.CompareTo(max) > 0) max = nilai3;
+            Console.WriteLine($"nilai tertinggi: {max}");
+
+
+        }   
+    }
+}
